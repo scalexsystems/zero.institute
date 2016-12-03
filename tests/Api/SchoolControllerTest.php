f8a5412cfe5eb577255f188db1f6be362dd948b6
Rahul Kadyan <hi@znck.me>
2016-12-03 12:46:06 +0530

    :sparkles: Add e2e tests

--- tests/Api/SchoolControllerTest.php
+++ tests/Api/SchoolControllerTest.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Scalex\Zero\Action;
 use Scalex\Zero\Models\School;
-use Scalex\Zero\User;
-use Znck\Trust\Models\Permission;
 
 class SchoolControllerTest extends TestCase
 {
@@ -15,29 +12,7 @@ class SchoolControllerTest extends TestCase
         factory(School::class, 10)->create();
 
         $this->json('GET', '/api/schools')
-            ->seeJsonStructure(
-                [
-                    'data' => [
-                        '*' => ['id', 'name', 'extra'],
-                    ],
-                    '_meta' => ['pagination'],
-                ]
-            );
-    }
-
-    // FIXME: add test_it_searches_schools
-
-    public function test_it_updates_school() {
-        /** @var User $user */
-        $user = factory(User::class)->create();
-
-        $user->permissions()->attach(
-            Permission::create(['slug' => Action::UPDATE_SCHOOL, 'name' => 'Update school information'])
-        );
-
-
-        $this->json('PUT', '/api/school', ['name' => 'Yo School!'])->seeStatusCode(202);
-
-        $this->seeInDatabase('schools', ['name' => 'Yo School!']);
+            ->seeJson()
+            ->seeJsonContains(transform(School::paginate()));
     }
 }
