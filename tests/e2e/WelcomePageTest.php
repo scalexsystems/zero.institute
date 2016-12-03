648026e7dc91a174a6764498474891dee91ecdaa
Rahul Kadyan <hi@znck.me>
2016-12-03 13:38:22 +0530

    Add e2e tests (#1)
    * :sparkles: Add e2e tests
    
    * :construction_worker: Add circle config
    
    * :raised_hands: Use PHP 7.1.0
    
    * :raised_hands: Use PHP 7.0.2
    
    * :raised_hands: Use PHP 7.0.4
    
    * :construction_worker: Try new config
    
    * :pencil2: Fix typo
    
    * :hankey: Remove invalid command
    
    * :raised_hands: Use .env.example
    
    * :pencil2: Fix type in config

---
+++ tests/e2e/WelcomePageTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class WelcomePageTest extends TestCase
+{
+    public function test_it_loads_welcome_page() {
+        $this->visit('/')
+             ->see('Zero')
+             ->click('Login')
+             ->seePageIs('/login');
+
+         $this->visit('/')
+              ->click('Register')
+              ->seePageIs('/register');
+    }
+}
