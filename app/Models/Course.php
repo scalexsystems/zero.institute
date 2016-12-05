9376e7672cca04677260029f84fc5b9aed61a36e
Rahul Kadyan <hi@znck.me>
2016-12-05 12:52:07 +0530

    :construction: Add course model

---
+++ app/Models/Course.php
@@ -0,0 +1,34 @@
+<?php namespace Scalex\Zero\Models;
+
+use Scalex\Zero\Database\BaseModel;
+use Scalex\Zero\Contracts\Database\BelongsToSchool;
+use Scalex\Zero\Models\Group;
+use Scalex\Zero\Models\School;
+use Scalex\Zero\Models\Department;
+use Scalex\Zero\Models\Discipline;
+use Scalex\Zero\Models\Attachment;
+
+class Course extends BaseModel implements BelongsToSchool
+{
+    protected $fillable = ['name', 'code', 'description'];
+
+    public function school() {
+        return $this->belongsTo(School::class);
+    }
+
+    public function department() {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function discipline() {
+        return $this->belongsTo(Discipline::class);
+    }
+
+    public function group() {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function photo() {
+        return $this->belongsTo(Attachment::class);
+    }
+}
