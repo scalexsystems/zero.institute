3c6a1aa97369d70a8b6cd5e9603b5f8b3fa8eb2b
Rahul Kadyan <hi@znck.me>
2016-12-05 13:05:39 +0530

    :construction: Add course transformer

---
+++ app/Transformers/CourseTransformer.php
@@ -0,0 +1,47 @@
+<?php namespace Scalex\Zero\Transformers;
+
+use Scalex\Zero\Models\Course;
+use Znck\Transformers\Transformer;
+
+
+class CourseTransformer extends Transformer
+{
+    protected $availableIncludes = [
+        'school',
+        'department',
+        'discipline',
+        'group',
+    ];
+
+    public function show(Course $course) {
+        return $this->index($course);
+    }
+
+    public function index(Course $course) {
+        return [
+            'name' => (string) $course->name,
+            'code' => (string) $course->code,
+            'photo' => (string) attach_url($group->photo) ?? asset('img/placeholder-64.jpg'),
+            'description' => (string) $course->description,
+            'department_id' => (int) $course->department_id,
+            'discipline_id' => (int) $course->discipline_id,
+            'group_id' => (int) $course->group_id,
+        ];
+    }
+
+    public function includeSchool(Course $course) {
+        return $this->item($course->school);
+    }
+
+    public function includeDepartment(Course $course) {
+        return $this->item($course->department);
+    }
+
+    public function includeDiscipline(Course $course) {
+        return $this->item($course->discipline);
+    }
+
+    public function includeGroup(Course $course) {
+        return $this->item($course->group);
+    }
+}
