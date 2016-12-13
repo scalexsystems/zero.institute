a01cc9151ad03ae76c8da28808e3777e163fc913
dylan_DPC <dylan.dpc@gmail.com>
2016-12-14 01:02:36 +0530

    file upload added

---
+++ app/Http/Controllers/Api/Groups/FileController.php
@@ -0,0 +1,36 @@
+<?php namespace Scalex\Zero\Http\Controllers\Api\Groups;
+
+
+use Ramsey\Uuid\Uuid;
+use Znck\Attach\Builder;
+use Illuminate\Http\Request;
+use Scalex\Zero\Models\Group;
+use Scalex\Zero\Models\Message;
+use Scalex\Zero\Http\Controllers\Controller;
+
+class FileController extends Controller
+{
+    /**
+     * Create new file.
+     * POST /groups/{group}/file
+     * Requires: auth
+     */
+    public function store(Request $request, Group $group) {
+        $this->authorize('upload-file', $group);
+        $this->validate($request, ['file' => 'required']);
+
+        $schoolId = $request->user()->school_id;
+        $groupId = $group->getKey();
+        $slug = Uuid::uuid4();
+        $path = "schools/${schoolId}/groups/${groupId}";
+
+        $file = Builder::make($request, 'file')
+            ->resize(300, null, 300)
+            ->upload(compact('slug', 'path'))
+            ->getAttachment();
+
+        if (!$file->exists) {
+            abort(500, 'Your file just broke our servers.');
+        }
+    }
+}
