184f26cb75cbba962a373638cb5391b41c3b7305
Rahul Kadyan <hi@znck.me>
2016-12-19 17:12:09 +0530

    :package: New build with v0.1.11 (#26)

--- resources/views/app/vue.blade.php
+++ resources/views/app/vue.blade.php
@@ -1,15 +1,15 @@
 @section('vue-scripts')
 
-<script src="/static/js/manifest.dfb365eca5194db2445d.js"></script>
+<script src="/static/js/manifest.0642459ae5fd524199c4.js"></script>
 
-<script src="/static/js/vendor.32a3b418b2ae1532af4d.js"></script>
+<script src="/static/js/vendor.e2ba247e2c6169cf1997.js"></script>
 
-<script src="/static/js/app.4de0caf3cdbe995b911d.js"></script>
+<script src="/static/js/app.698ec4515c51b777a6ae.js"></script>
 
 @endsection
 
 @section('vue-styles')
     
-    <link href="/static/css/app.e3594a98e8d9de2078b4dc9eac71750f.css" rel="stylesheet">
+    <link href="/static/css/app.73ac7c60010ed726912b47b753ded06f.css" rel="stylesheet">
     
 @endsection
