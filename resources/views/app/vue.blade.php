032cf0a0a8a2382eb65881875a6f1c0eee67c088
Rahul Kadyan <hi@znck.me>
2017-01-21 12:20:02 +0530

    :package: Version 0.2.0

--- resources/views/app/vue.blade.php
+++ resources/views/app/vue.blade.php
@@ -1,15 +1,15 @@
 @section('vue-scripts')
 
-<script src="/static/js/manifest.40ef1e33508d83c3d89e.js"></script>
+<script src="/static/js/manifest.d2960c2a6d00015e48cf.js"></script>
 
-<script src="/static/js/vendor.aa1e768d9d147d24040a.js"></script>
+<script src="/static/js/vendor.e6c947b86564cbe21c30.js"></script>
 
-<script src="/static/js/app.96e3a5525409a15d9805.js"></script>
+<script src="/static/js/app.ec24adcd5c713e331840.js"></script>
 
 @endsection
 
 @section('vue-styles')
     
-    <link href="/static/css/app.592fde2c72542cd04efdabb37b95fdd6.css" rel="stylesheet">
+    <link href="/static/css/app.51e437197043ef0ef3575fe30fcca32f.css" rel="stylesheet">
     
 @endsection
