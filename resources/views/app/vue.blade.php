dd4595eb2fbe8abd859f8e978a50a08392f597be
Rahul Kadyan <hi@znck.me>
2017-01-22 15:10:41 +0530

    :package: Build for production using front v0.2.1 (#65)

--- resources/views/app/vue.blade.php
+++ resources/views/app/vue.blade.php
@@ -1,15 +1,15 @@
 @section('vue-scripts')
 
-<script src="/static/js/manifest.d2960c2a6d00015e48cf.js"></script>
+<script src="/static/js/manifest.2499a813874d96287ca6.js"></script>
 
 <script src="/static/js/vendor.e6c947b86564cbe21c30.js"></script>
 
-<script src="/static/js/app.ec24adcd5c713e331840.js"></script>
+<script src="/static/js/app.4db7904f668a6441f24c.js"></script>
 
 @endsection
 
 @section('vue-styles')
     
-    <link href="/static/css/app.51e437197043ef0ef3575fe30fcca32f.css" rel="stylesheet">
+    <link href="/static/css/app.6663087a8f43ef3eb951530e46c6b553.css" rel="stylesheet">
     
 @endsection
