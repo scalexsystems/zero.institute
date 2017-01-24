e21b77ec72b586dceb404fe7f60724574c0341aa
Rahul Kadyan <hi@znck.me>
2017-01-24 16:43:12 +0530

    :sparkles: Add user service

---
+++ app/Services/UserService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Scalex\Zero\Services;
+use Illuminate\Http\Request;
+use Scalex\Zero\User;
+
+class UserService {
+    public function current() {
+        return app(Request::class)->user();
+    }
+
+    public function currentTransformed() {
+        return transform($this->current(), ['person']);
+    }
+}
