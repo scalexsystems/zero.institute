c6c8bb1df13de6d4743a18dcc9fcdf651be56614
Rahul Kadyan <hi@znck.me>
2017-02-21 18:26:29 +0530

    Add build script

---
+++ scripts/production.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+rm -rf public/js public/css public/fonts public/vendor.*.js public/manifest.*.js public/vendor.*.js.map public/manifest.*.js.map || true
+yarn run build:prod
+git add --force public
+git commit -m "Build ${PACKAGE_VERSION}"
