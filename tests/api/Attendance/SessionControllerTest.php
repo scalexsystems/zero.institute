271ae14acc30154694c7e075d8f5885acdd1304b
dylan_DPC <dylan.dpc@gmail.com>
2017-02-24 20:36:45 +0530

    minor fix

--- tests/api/Attendance/SessionControllerTest.php
+++ tests/api/Attendance/SessionControllerTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Test\Api\Attendance\AttendanceTestHelper;
 use Test\Api\People\Students\StudentTestHelper;
 
 class SessionControllerTest extends TestCase
