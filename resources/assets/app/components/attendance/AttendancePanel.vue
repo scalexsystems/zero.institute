0677f5b218f5bef88be5004eba27714658476ec0
dylan_DPC <dylan.dpc@gmail.com>
2017-02-28 19:56:26 +0530

    list component added

--- resources/assets/app/components/attendance/AttendancePanel.vue
+++ resources/assets/app/components/attendance/AttendancePanel.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
-        <div class="card">
-            <div class="card-header">
-                Attendance
-            </div>
-
-            <div class="card-block">
-                <div class="row">
-                    <div class="col-6 col-lg-6">
-                        <input-select title="Semester" v-model.number="semester_id" :feedback="errors.semester_id" :options="semesters" />
-                        <list :items="sessions" @listClicked="sessionClicked">
-
-                        </list>
-                    </div>
-                </div>
-            </div>
+      <div class="card">
+        <div class="card-header">
+            Attendance
         </div>
-    </div>
+
+        <div class="card-block">
+           <div class="row">
+             <div class="col-6 col-lg-6">
+                 <input-select title="Semester" v-model.number="semester_id" :feedback="errors.semester_id" :options="semesters" />
+                 <list :items="sessions" @listClicked="sessionClicked">
+
+                 </list>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
 </template>
 
 <script lang="babel">
@@ -26,24 +26,24 @@
     export default {
         name: 'AttendancePanel',
         data: () => [{
-            activeSession: null
+          activeSession: null
         }],
         computed: {
-            items() {
-                const sessions = this.sessions;
-                sessions.forEach(session => {
-                })
+          items() {
+           const sessions = this.sessions;
+           sessions.forEach(session => {
+           })
 
 
-            },
-            ...mapGetters('courses', ['sessions']),
+          },
+        ...mapGetters('courses', ['sessions']),
 
         },
 
         methods: {
-            sessionClicked(event, session){
-                this.activeSession = session;
-            }
+          sessionClicked(event, session){
+            this.activeSession = session;
+          }
         }
 
     }
---
+++ resources/assets/app/components/attendance/List.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="list-group">
+            <a role="button" class="list-group-item list-group-item-action" v-for="item in items" @click="itemClicked(item)">
+                {{ item.name }}
+            </a>
+            <div>
+            </div>
+</template>
+
+<script lang="babel">
+
+    export default {
+        name: 'List',
+
+        props: {
+            items: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            itemClicked(event, item){
+                this.$emit('listClicked', item);
+            }
+        }
+
+        components: { SidebarButton }
+    }
+</script>
