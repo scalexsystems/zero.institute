9307755b10341094aef1de9d32e06c6f42dda4d0
dylan_DPC <dylan.dpc@gmail.com>
2017-04-27 14:16:47 +0530

    razorpay added, initial code

---
+++ resources/assets/app/pages/payment/Checkout.vue
@@ -0,0 +1,52 @@
+<template>
+    <button id="rzp-button1" @click="pay">Pay</button>
+</template>
+
+<script lang="babel">
+export default {
+    data: () => ({
+        options: {
+            "key": "YOUR_KEY_ID",
+            "amount": "2000", // 2000 paise = INR 20
+            "name": "Merchant Name",
+            "description": "Purchase Description",
+            "image": "/your_logo.png",
+            "handler": function (response) {
+                alert(response.razorpay_payment_id);
+            },
+            "prefill": {
+                "name": "Harshil Mathur",
+                "email": "harshil@razorpay.com"
+            },
+            "notes": {
+                "address": "Hello World"
+            },
+            "theme": {
+                "color": "#F37254"
+            }
+        }
+    }),
+
+    computed: {
+
+    },
+
+
+    methods: {
+        pay() {
+          const razorpay = new Razorpay(this.options);
+          razorpay.open();
+        }
+
+    }
+
+
+
+
+};
+
+</script>
+
+<style lang="scss">
+
+</style>
